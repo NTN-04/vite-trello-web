a99818b8e2864b9f4311b0c3187724307e49f923
NTN-04 <nghianguyen2004719@gmail.com>
2025-04-23 11:06:22 +0700

    select dark light mode p2

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,52 @@ import Typography from '@mui/material/Typography'
 
 import { useColorScheme } from '@mui/material/styles'
 
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import Box from '@mui/material/Box'
+
+import LightModeIcon from '@mui/icons-material/LightMode'
+import DarkModeIcon from '@mui/icons-material/DarkMode'
+import SettingsSuggestIcon from '@mui/icons-material/SettingsSuggest'
+
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme()
+  const handleChange = event => {
+    const selectMode = event.target.value
+    setMode(selectMode)
+  }
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="select-dark-light-mode">Mode</InputLabel>
+      <Select
+        labelId="select-dark-light-mode"
+        id="dark-light-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value="light">
+          <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+            <LightModeIcon fontSize="small" /> Light
+          </div>
+        </MenuItem>
+        <MenuItem value="dark">
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <DarkModeIcon fontSize="small" /> Dark
+          </Box>
+        </MenuItem>
+        <MenuItem value="system">
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <SettingsSuggestIcon fontSize="small" /> System
+          </Box>
+        </MenuItem>
+      </Select>
+    </FormControl>
+  )
+}
 // lưu localStore
 function ModeToggle() {
   const { mode, setMode } = useColorScheme()
@@ -25,6 +71,8 @@ function ModeToggle() {
 function App() {
   return (
     <>
+      <ModeSelect />
+      <hr />
       <ModeToggle />
       <hr />
       <div>nguyên trong nghia</div>
